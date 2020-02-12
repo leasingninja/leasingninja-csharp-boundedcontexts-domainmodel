f15636a990febfbed210b365fd8b33071ba0e045
Henning Schwentner <hs@wps.de>
2020-02-12 08:34:12 +0100

    Added InMemoryContracts

---
+++ src/LeasingNinja.Sales/Infrastructure/InMemoryContracts.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using LeasingNinja.Sales.Domain;
+
+namespace LeasingNinja.Sales.Infrastructure
+{
+    public class InMemoryContracts : Contracts
+    {
+        private readonly IDictionary<ContractNumber, Contract> _contracts;
+	
+        public InMemoryContracts() {
+            _contracts = new Dictionary<ContractNumber, Contract>();
+        }
+
+        public Contract With(ContractNumber number)
+        {
+            return _contracts[number];
+        }
+
+        public void Save(Contract contract)
+        {
+            _contracts.Add(contract.Number, contract);
+        }
+    }
+}
---
+++ tests/LeasingNinja.Sales.Test/Infrastructure/InMemoryContractsTest.cs
@@ -0,0 +1,34 @@
+using LeasingNinja.Sales.Domain;
+using NFluent;
+using Xunit;
+
+namespace LeasingNinja.Sales.Infrastructure
+{
+    public class InMemoryContractsTest
+    {
+        private InMemoryContracts _contractsUnderTest;
+	
+        [Fact]
+        void GivenEmtpyContracts_WhenContractSavedAndLoaded_ThenItIsAnEqualContract() {
+            // given
+            _contractsUnderTest = new InMemoryContracts();
+		
+            // when
+            _contractsUnderTest.Save(new Contract(
+                ContractNumber.Of("4711"),
+                Customer.Of("John Buyer"),
+                Car.Of("Mercedes Benz C class"),
+                Amount.Of(20_000, "EUR")));
+            var contract = _contractsUnderTest.With(ContractNumber.Of("4711"));
+		
+            // then
+            Check.That(contract).HasFieldsWithSameValues(
+                new Contract(
+                    ContractNumber.Of("4711"),
+                    Customer.Of("John Buyer"),
+                    Car.Of("Mercedes Benz C class"),
+                    Amount.Of(20_000, "EUR")));
+        }
+        
+    }
+}
