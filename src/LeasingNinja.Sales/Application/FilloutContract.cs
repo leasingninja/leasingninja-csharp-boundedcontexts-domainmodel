041e5def9689f2a23b3c061d970c46dc83226f7b
Henning Schwentner <hs@wps.de>
2020-02-12 17:28:53 +0100

    Added use case FilloutContract

---
+++ src/LeasingNinja.Sales/Application/FilloutContract.cs
@@ -0,0 +1,23 @@
+using LeasingNinja.Sales.Domain;
+
+namespace LeasingNinja.Sales.Application
+{
+    public class FilloutContract
+    {
+        private readonly Contracts _contracts;
+
+        public FilloutContract(Contracts contracts)
+        {
+            _contracts = contracts;
+        }
+
+        public void With(ContractNumber number, Customer customer, Car car, Amount amount)
+        {
+            _contracts.Save(new Contract(
+                number, 
+                customer, 
+                car,
+                amount));        
+        }
+    }
+}
