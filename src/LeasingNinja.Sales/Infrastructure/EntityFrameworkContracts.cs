0099d57ec0eca18c500eed5769fb629ba7cca6b9
Henning Schwentner <hs@wps.de>
2022-02-22 13:00:29 +0100

    EF repository stub implementation

---
+++ src/LeasingNinja.Sales/Infrastructure/EntityFrameworkContracts.cs
@@ -0,0 +1,17 @@
+using LeasingNinja.Sales.Domain;
+
+namespace LeasingNinja.Sales.Infrastructure
+{
+    public class EntityFrameworkContracts : Contracts
+    {
+        public Contract With(ContractNumber number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(Contract contract)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
