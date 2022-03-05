49dfb8481be219da43ab07ccaafec5ff21a02eca
Henning Schwentner <hs@wps.de>
2022-03-05 08:12:28 +0100

    Make Amount a readonly record struct

--- src/LeasingNinja.Sales/Domain/Amount.cs
+++ src/LeasingNinja.Sales/Domain/Amount.cs
@@ -3,22 +3,8 @@
 namespace LeasingNinja.Sales.Domain
 {
     [ValueObject]
-//    public readonly record struct Amount(decimal AmountValue, string Currency);
-    public struct Amount
+    public readonly record struct Amount(decimal AmountValue, string Currency)
     {
-        public decimal AmountValue { get; }
-        public string Currency { get; }
-
-        private Amount(decimal amount, string currency) : this()
-        {
-            AmountValue = amount;
-            Currency = currency;
-        }
-
-        public static Amount Of(decimal amount, string currency) {
-            //assert currency != null;
-		
-            return new Amount(amount, currency);
-        }
+        public static Amount Of(decimal amount, string currency) => new Amount(amount, currency);
     }
 }
