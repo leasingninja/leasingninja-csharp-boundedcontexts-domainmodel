ffe4504bf01b86a545f0b38d746550b0fa07eb7b
Henning Schwentner <hs@wps.de>
2022-11-29 23:50:42 +0100

    Add enum Currency

---
+++ src/LeasingNinja.Sales/Domain/Currency.cs
@@ -0,0 +1,10 @@
+using NMolecules.DDD;
+
+namespace LeasingNinja.Sales.Domain
+{
+    [ValueObject]
+    public enum Currency
+    {
+        EUR, GBP, USD
+    }
+}
---
+++ tests/LeasingNinja.Sales.Test/Domain/CurrencyTest.cs
@@ -0,0 +1,22 @@
+using NFluent;
+using Xunit;
+
+namespace LeasingNinja.Sales.Domain
+{
+    public class CurrencyTest
+    {
+        [Fact]
+        void givenTwoUnequalCurrencies_whenEquals_thenAreNotEqual()
+        {
+            // given
+            var currency1 = Currency.EUR;
+            var currency2 = Currency.USD;
+
+            // when
+            bool areEqual = currency1.Equals(currency2);
+
+            // then
+            Check.That(areEqual).IsFalse();
+        }
+    }
+}
