ea6522390770670dd00f529897fb6f208deed09d
Henning Schwentner <hs@wps.de>
2022-12-01 19:34:24 +0100

    Remove unused dddbits

--- src/dddbits/Basetypes/TinyStringValue.cs
+++
@@ -1,9 +0,0 @@
-namespace dddbits.Basetypes
-{
-    public class TinyStringValue : TinyValue<string>
-    {
-        protected TinyStringValue(string value) : base(value)
-        {
-        }
-    }
-}
